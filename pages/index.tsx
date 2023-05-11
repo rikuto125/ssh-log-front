501d27a7d21cdce5091fad1a849251e171851461
ta1ca <takaakiohori@gmail.com>
2023-05-11 21:40:25 +0900

    front

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ interface IPAddress {
   count: number;
   ip_address: string;
   country: string | null;
+  hostname: string;
 }
 
 const IndexPage = () => {
@@ -16,6 +17,7 @@ const IndexPage = () => {
   const fetchData = async (data: string) => {
     try {
       const response = await axios.get(`http://127.0.0.1:10000/api/top-ten-command?date=${data}`);
+      console.log(response.data)
       setIpAddresses(response.data.result);
     } catch (error) {
       console.error(error);
@@ -56,12 +58,34 @@ const IndexPage = () => {
             </TableHead>
             <TableBody>
               {ipAddresses.map((ip, index) => (
-                  <TableRow key={index}>
+                  ip.hostname === 'hanypod' ? <TableRow key={index}>
                     <TableCell>{ip.rank}</TableCell>
                     <TableCell>{ip.count}</TableCell>
                     <TableCell>{ip.ip_address}</TableCell>
                     <TableCell>{ip.country || '不明'}</TableCell>
-                  </TableRow>
+                  </TableRow> : null
+              ))}
+            </TableBody>
+            </Table>
+         </Paper>
+        <Paper style={{ marginTop: '1rem' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>順位</TableCell>
+                <TableCell>回数</TableCell>
+                <TableCell>IPアドレス</TableCell>
+                <TableCell>国</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {ipAddresses.map((ip, index) => (
+                  ip.hostname === 'cririn' ? <TableRow key={index}>
+                    <TableCell>{ip.rank}</TableCell>
+                    <TableCell>{ip.count}</TableCell>
+                    <TableCell>{ip.ip_address}</TableCell>
+                    <TableCell>{ip.country || '不明'}</TableCell>
+                  </TableRow> : null
               ))}
             </TableBody>
           </Table>
